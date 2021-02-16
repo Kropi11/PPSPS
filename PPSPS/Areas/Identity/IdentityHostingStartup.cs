534a3d30843d62e87ee56406a1e2dd482a0662e2
Richard Kropáček <risakropacek@seznam.cz>
2021-02-16 23:37:09 +0100

    Přihlašování, Registrace

---
+++ PPSPS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PPSPS.Areas.Identity.Data;
+using PPSPS.Data;
+
+[assembly: HostingStartup(typeof(PPSPS.Areas.Identity.IdentityHostingStartup))]
+namespace PPSPS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDBContext>(options =>
+                    options.UseMySQL(
+                        context.Configuration.GetConnectionString("AuthDBContextConnection")));
+
+                services.AddDefaultIdentity<PPSPSUser>(options =>
+                    {
+                        options.SignIn.RequireConfirmedAccount = true;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                        options.Password.RequireNonAlphanumeric = false;
+                    })
+                    .AddEntityFrameworkStores<AuthDBContext>();
+            });
+        }
+    }
+}
