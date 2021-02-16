c7e9c65bfdbf6548363ae1900c0f3f550424a01f
Richard Kropáček <risakropacek@seznam.cz>
2021-02-16 23:35:47 +0100

    Přihlašování, Registrace

---
+++ PPSPS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PPSPS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
