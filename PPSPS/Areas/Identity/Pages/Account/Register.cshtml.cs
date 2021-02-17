bd1b4992a1a375adad1e2199136d3365e80be68a
Richard Kropáček <risakropacek@seznam.cz>
2021-02-17 09:27:57 +0100

    Registrace, Přihlašování

--- PPSPS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PPSPS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -94,7 +94,7 @@ namespace PPSPS.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    _logger.LogInformation("User created a new account with  password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
