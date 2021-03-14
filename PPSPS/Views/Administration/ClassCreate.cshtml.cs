813e4df83346320b72bcb7471790ae6ede5ba912
Richard Kropáček <risakropacek@seznam.cz>
2021-03-14 21:18:12 +0100

    Zobrazování času.

---
+++ PPSPS/Views/Administration/ClassCreate.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PPSPS.Views.Administration
+{
+    public class ClassAdd : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
